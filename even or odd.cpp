3d885d4a9c37078259e04f3164b5696a80bade20
Sofia Jasmine <sofiavincent03@gmail.com>
2025-06-30 19:47:46 +0530

    Update even or odd.cpp

--- even or odd.cpp
+++ even or odd.cpp
@@ -1,3 +1,14 @@
+// Program: Even and Odd Numbers
+// Description: Takes a number n as input from the user and prints whether each number from 1 to n is even or odd. 
+// Input Example: 6
+// Output Example: Even and Odd numbers from 1 to 6:
+                    1 is Odd
+                    2 is Even
+                    3 is Odd
+                    4 is Even
+                    5 is Odd
+                    6 is Even
+
 #include <iostream>
 using namespace std;
 
