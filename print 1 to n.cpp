0710926cab530f506b5bc6c65ec1fad0bade7f5c
Sofia Jasmine <sofiavincent03@gmail.com>
2025-07-02 09:26:25 +0530

    Add files via upload

---
+++ print 1 to n.cpp
@@ -0,0 +1,26 @@
+// Program: Print 1 to n
+// Description: Prints 1 to n using a loop.
+// Input Example: 5
+/* Output Example: 1
+                   2
+                   3
+                   4
+                   5
+*/
+
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter a Number: " << endl;
+    cin >> n;
+    for (int i=1;i<=n;i++){
+        cout << i << endl;
+    }
+    return 0;
+}
